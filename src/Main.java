043c673ef3f06cbedfd043f6ce8a674b82237103
sharayura <sharapyura@gmail.com>
2022-08-03 16:59:08 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {  task1();    }
+    public static void task1() {
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
 }
