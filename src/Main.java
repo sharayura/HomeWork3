67a04944f908e2f03713007c6e843339ea9a1008
sharayura <sharapyura@gmail.com>
2022-08-03 17:20:16 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
 public class Main {
-    public static void main(String[] args) {  task1();    }
-    public static void task1() {
+    public static void main(String[] args) { task2();    }
+    public static void task2() {
         int clientOS = 1;
+        int clientDeviceYear = 2015;
         if (clientOS == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
         }
-        else {
+        else if (clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+
     }
 }
