aac577f0f062104ea69b5a947a6b2ef29d74671d
sharayura <sharapyura@gmail.com>
2022-08-04 21:22:52 +0300

    task3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task3();
+    }
+
+    public static void task3() {
+        int year = 2100;
+        if ((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
 }
