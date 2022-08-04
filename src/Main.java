49672d63bb94586300119ea99c292005d0731095
sharayura <sharapyura@gmail.com>
2022-08-04 23:13:50 +0300

    task6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task6();
+    }
+    public static void task6() {
+        int age = 19;
+        int salary = 58_000;
+        int credit;
+        if (age< 23) { credit = salary * 2; }
+        else credit = salary * 3;
+        if (salary >= 50_000 && salary < 80_000) { credit *= 1.2; }
+        if (salary >= 80_000 ) { credit *= 1.5; }
+        System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + credit + " рублей");
     }
 }
