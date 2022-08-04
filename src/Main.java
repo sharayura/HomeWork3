cb559a3564b5f89ed5ab87a364ac98ae3075eb6b
sharayura <sharapyura@gmail.com>
2022-08-04 21:37:03 +0300

    task4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task4();
+    }
+    public static void task4() {
+        int deliveryDistance = 95;
+        int days = 1;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней: " + days);
+        }
+        if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            days+=1;
+            System.out.println("Потребуется дней: " + days);
+        }
+        if (deliveryDistance >= 60 && deliveryDistance < 100) {
+            days+=2;
+            System.out.println("Потребуется дней: " + days);
+        }
     }
 }
